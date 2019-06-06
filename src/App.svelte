9a843f1acb5f2b50c2b3d8cd4bb8a40d3d9fcaba
Yuri Toledo <yuriwtoledo@gmail.com>
2019-06-05 22:46:32 -0300

    Adicionado unas cocitas mas

--- src/App.svelte
+++ src/App.svelte
@@ -1,5 +1,12 @@
 <script>
-	import Form from './Form.svelte'
-	let name;
+  import Form from "./Pages/Form.svelte";
+  import Counter from "./Pages/Counter.svelte";
+  import { counter } from "./Store/store";
+
+  let counterValue;
+  counter.subscribe(val => (counterValue = val));
 </script>
+
 <Form />
+<Counter />
+<h1>Contador: {counterValue}</h1>
